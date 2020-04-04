1f965bd46d9dcb7226542a75d6167260ba9fbc40
Prasanth <prasanthc70@gmail.com>
2020-04-04 17:12:56 +0530

    creates student,classes and classes_student tables and mapping successfully - have some constraints

--- src/com/learner_academy/model/Classes.java
+++ src/com/learner_academy/model/Classes.java
@@ -2,7 +2,6 @@ package com.learner_academy.model;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
